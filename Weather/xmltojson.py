981990259e581ff0f52dad2b4f762e4d8dfc80ad
Thun Thunkijjanukij <44374820+Klan300@users.noreply.github.com>
2019-01-28 23:22:23 +0700

    Update xmltojson.py

--- Weather/xmltojson.py
+++ Weather/xmltojson.py
@@ -3,6 +3,9 @@ import json
 
 
 def create_file_name(filename):
+    """
+    change name from xml to json name 
+    """
     sep = filename.split('.')
     name = './' + './' + '/' + sep[0] + '.json'
     return name, sep
@@ -16,6 +19,3 @@ with open(name, 'w') as fp:
     json.dump(diction, fp, indent=4)
 
 print(sep[0] + '.json')
-
-# https://www.google.co.th/search?ei=8M1NXOCyNovevASdo4zoBQ&q=how+to+write+json+file+in+python&oq=how+to+write+js&gs_l=psy-ab.3.1.0l10.3654.9508..12172...1.0..0.138.1426.12j4......0....1..gws-wiz.......0i71j0i131i67j0i67j0i131.4Mlgl9uNJwg#kpvalbx=1
-# https://stackoverflow.com/questions/17043860/python-dump-dict-to-json-file
